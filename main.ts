a47dd4dd5df27d10fcdb48b5dcb3396cf664f67f
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-05-23 12:04:20 +0200

    werkend

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 3; index++) {
         basic.showIcon(IconNames.Heart)
         basic.pause(200)
+        basic.clearScreen()
     }
     basic.clearScreen()
 })
